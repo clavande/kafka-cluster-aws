b7f6487fd7ea43812e3788b76dfa3a03ed4cf288
Chinmay Lavande <clavande@verticalrelevance.com>
2026-04-22 15:50:18 +0530

    style: fixed terrafrom formatting

--- tf/secrets.tf
+++ tf/secrets.tf
@@ -7,7 +7,7 @@ resource "aws_secretsmanager_secret" "kafka_auth" {
 }
 
 resource "aws_secretsmanager_secret_version" "kafka_auth_v1" {
-  secret_id     = aws_secretsmanager_secret.kafka_auth.id
+  secret_id = aws_secretsmanager_secret.kafka_auth.id
   secret_string = jsonencode({
     username = "admin"
     password = "very-secret-password-123"
